eab4cddc38e2456ddeb695a7c590823f00b1a4a3
chaeyoon sung <105664139+itscodbs@users.noreply.github.com>
2023-02-08 20:13:45 +0900

    commit

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -40,16 +40,16 @@ public class Board {
 	private String title; 
 	
 	@Lob // 대용량 데이터
-	private String content; // 섬머노트 라이브러리 <html> 태그가 섞여서 디자인됨
+	private String content; 
 	
 	@ColumnDefault("0")
-	private int count; // 조회수
+	private int count; 
 	
 	@ManyToOne(fetch = FetchType.EAGER) // Many = many, User= one
 	@JoinColumn(name="userId")
-	private User user; // DB는 오브젝트를 저장할 수 없다. FK, java는 오브젝트를 저장할 수 있다.
+	private User user; 
 	
-	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE) 
 	@JsonIgnoreProperties({"board"})
 	@OrderBy("id desc")
 	private List<Reply> replys;
